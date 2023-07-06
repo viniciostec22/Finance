e5613851d943afa1af5b3e9d9a30eaf2f31ba78c
VINICIOS MATHEUS OLIVEIRA DA SILVA <106559003+viniciostec22@users.noreply.github.com>
2023-07-06 20:11:06 -0300

    init

---
+++ perfil/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('home/', views.home, name='home'),
+    path('gerenciar/', views.gerenciar, name='gerenciar'),
+    path('cadastrar_banco/', views.cadastrar_banco, name='cadastrar_banco'),
+]
---
+++ perfil/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render, redirect
+from . models import *
+
+def home(request):
+    return render(request, 'home.html')
+
+def gerenciar(request):
+    return render(request, 'gerenciar.html')
+
+def cadastrar_banco(request):
+    apelido = request.POST.get('apelido')
+    banco = request.POST.get('banco')
+    tipo = request.POST.get('tipo')
+    valor = request.POST.get('valor')
+    icone = request.FILES.get('icone')
+    
+    if len(apelido.strip()) == 0 or len(valor.strip()) == 0:
+        return redirect('/perfil/gerenciar/')
+    
+    conta = Conta(
+        apelido = apelido,
+        banco=banco,
+        tipo=tipo,
+        valor=valor,
+        icone=icone
+    )
+
+    conta.save()
+
+    return redirect('/perfil/gerenciar/')
